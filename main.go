ce7bfed702e7c7877ae657ec48d34a339ba13aee
Abu <maikoabraham68@gmail.com>
2024-04-11 14:10:35 +0300

    updated main.go

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func Vowel(s string) string {
-
 	// Every instance of a should be turned into an if the next word begins with a vowel (a, e, i, o, u) or a h.
 	//  (Ex: "There it was. A amazing rock!" -> "There it was. An amazing rock!").
 
@@ -38,26 +37,24 @@ func Vowel(s string) string {
 	return strings.TrimSpace(result)
 }
 
-
 func Punctuation(s string) string {
-
-	// The punctuation mark ' will always be found with another instance of it and they should be placed to the 
+	// The punctuation mark ' will always be found with another instance of it and they should be placed to the
 	// right and left of the word in the middle of them, without any spaces.
 	//  (Ex: "I am exactly how they describe me: ' awesome '" -> "I am exactly how they describe me: 'awesome'")
 
 	words := strings.Split(s, "'")
 	for i := range words {
 		words[i] = strings.TrimSpace(words[i])
+		// fmt.Println(words[i])
 	}
 	return strings.Join(words, "'")
 }
 
-
 func main() {
-	// input := "it (cap) was the best of TIMES (low, 3) , it was the worst of times (cap, 7), it was the age of wisdom, it was the age of foolishness Hello World  (up, 10) "
+	input := "it (up) was the best of TIMES (up, 3) , it was the worst of times (cap, 7), it was the age of wisdom, it was the age of foolishness Hello World  (up, 10) "
 	// input := "Simply add 42 (hex) and 10 (bin) and you will see the result is 68."
 	// input := "There is no greater agony than bearing a untold story inside you."
-	input := "I am exactly how they describe me: ' awesome '" 
+	// input := "I am exactly how they describe me: ' awesome  don't awesome'"
 
 	EditedInput := Punctuation(Vowel(input))
 
@@ -67,102 +64,100 @@ func main() {
 		if word != "" {
 			if word == "(cap)" {
 				words[i-1] = strings.Title(words[i-1])
-				words = append(words[:i], words[i+1:]... )
+				words = append(words[:i], words[i+1:]...)
 			} else if word == "(low)" {
 				words[i-1] = strings.ToLower(words[i-1])
-				words = append(words[:i], words[i+1:]... )
+				words = append(words[:i], words[i+1:]...)
 			} else if word == "(up)" {
 				words[i-1] = strings.ToUpper(words[i-1])
-				words = append(words[:i], words[i+1:]... )
-			} else if word == "(up," {
-				w := strings.Trim(strings.Trim(words[i+1], words[i][1:]), ")")
+				words = append(words[:i], words[i+1:]...)
+			} else if word == "(up," && i < len(words) {
+				w := strings.Trim(words[i+1], ")")
+				// fmt.Println(w)
+
 				num, error := strconv.Atoi(w)
 
 				if error != nil {
 					panic(error)
 				}
 
-				// if num > i {
-				// 	len := 
-				// }
+				// 			// if num > i {
+				// 			// 	len :=
+				// 			// }
 
 				for j := 0; j <= num; j++ {
-					if words[i-j] != ","{
-						words[i-j] = strings.ToUpper(words[i-j])
-					}
-					
+					words[i-j] = strings.ToUpper(words[i-j])
 				}
 
-				words = append(words[:i], words[i+2:]... )
+				words = append(words[:i], words[i+2:]...)
 
-				// fmt.Println(w)
+				// 			// fmt.Println(w)
 			} else if word == "(low," {
-				w := strings.Trim(strings.Trim(words[i+1], words[i][1:]), ")")
-				num, error := strconv.Atoi(w)
+						w := strings.Trim(strings.Trim(words[i+1], words[i][1:]), ")")
+						num, error := strconv.Atoi(w)
 
-				if error != nil {
-					panic(error)
-				}
+						if error != nil {
+							panic(error)
+						}
 
-				// if num > i {
-				// 	len := 
-				// }
+						// if num > i {
+						// 	len :=
+						// }
 
-				for j := 0; j <= num; j++ {
-					if words[i-j] != ","{
-						words[i-j] = strings.ToLower(words[i-j])
-					}
-					
-				}
-
-				words = append(words[:i], words[i+2:]... )
-
-				// fmt.Println(w)
-			} else if word == "(cap," {
-				w := strings.Trim(strings.Trim(words[i+1], words[i][1:]), ")")
-				num, error := strconv.Atoi(w)
-
-				if error != nil {
-					panic(error)
-				}
-
-				// if num > i {
-				// 	len := 
-				// }
-
-				for j := 0; j <= num; j++ {
-					if words[i-j] != ","{
-						words[i-j] = strings.Title(words[i-j])
-					}
-					
-				}
-
-				words = append(words[:i], words[i+2:]... )
-
-				// fmt.Println(w)
-			} else if word == "(hex)" {
-				num, error := strconv.ParseInt(words[i-1], 16, 64)
-
-				if error != nil {
-					panic(error)
-				}
-
-				words[i-1] = fmt.Sprint(num)
-
-				words = append(words[:i], words[i+1:]... )
-
-			}  else if word == "(bin)" {
-				num, error := strconv.ParseInt(words[i-1], 2, 64)
-
-				if error != nil {
-					panic(error)
-				}
-
-				words[i-1] = fmt.Sprint(num)
-
-				words = append(words[:i], words[i+1:]... )
-			}
+						for j := 0; j <= num; j++ {
+							if words[i-j] != ","{
+								words[i-j] = strings.ToLower(words[i-j])
+							}
 		}
+
+					words = append(words[:i], words[i+2:]... )
+
+					// fmt.Println(w)
+				} else if word == "(cap," {
+					w := strings.Trim(strings.Trim(words[i+1], words[i][1:]), ")")
+					num, error := strconv.Atoi(w)
+
+					if error != nil {
+						panic(error)
+					}
+
+					// if num > i {
+					// 	len :=
+					// }
+
+					for j := 0; j <= num; j++ {
+						if words[i-j] != ","{
+							words[i-j] = strings.Title(words[i-j])
+						}
+
+					}
+
+					words = append(words[:i], words[i+2:]... )
+
+					// fmt.Println(w)
+				} else if word == "(hex)" {
+					num, error := strconv.ParseInt(words[i-1], 16, 64)
+
+					if error != nil {
+						panic(error)
+					}
+
+					words[i-1] = fmt.Sprint(num)
+
+					words = append(words[:i], words[i+1:]... )
+
+				}  else if word == "(bin)" {
+					num, error := strconv.ParseInt(words[i-1], 2, 64)
+
+					if error != nil {
+						panic(error)
+					}
+
+					words[i-1] = fmt.Sprint(num)
+
+					words = append(words[:i], words[i+1:]... )
+				}
+			}
 	}
 
 	fmt.Println(strings.Join(words, " "))
