f6598517ee54a0b3704c66ddde4fb07d1ef15aca
abraham <maikoabraham68@gmail.com>
2024-04-15 14:54:12 +0300

    Update main.go

--- main_test.go
+++ main_test.go
@@ -47,6 +47,7 @@ func TestVowelsFunctionality(t *testing.T) {
 	}{
 		{"There is no greater agony than bearing a untold story inside you.", "There is no greater agony than bearing an untold story inside you."},
 		{"There it was. A amazing rock!", "There it was. An amazing rock!"},
+		{"There is no greater agony than bearing a untold story inside you.", "There is no greater agony than bearing an untold story inside you."},
 	}
 
 	for _, vowel := range vowels {
@@ -68,6 +69,7 @@ func TestProcessWordCommandsFunctionality(t *testing.T)  {
 		{"it was the worst of times (up) ", "it was the worst of TIMES"},
 		{"it was the age of wisdom, it was the age of foolishness (cap, 6) ", "it was the age of wisdom, It Was The Age Of Foolishness" },
 		{"it was the spring of hope, IT WAS THE (low, 3) winter of despair.", "it was the spring of hope, it was the winter of despair."},
+		{"Simply add 42 (hex) and 10 (bin) and you will see the result is 68.", "Simply add 66 and 2 and you will see the result is 68."},
 	}
 
 	for _, line := range commands {
